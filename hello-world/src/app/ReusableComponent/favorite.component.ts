9259627cc4efd2ae18e5de4ed73f6229bcfb1b53
Ruchir Saxena <ruchirsaxena@gmail.com>
2017-10-25 16:59:05 +0530

    Reusable Component : @Output

--- hello-world/src/app/ReusableComponent/favorite.component.ts
+++ hello-world/src/app/ReusableComponent/favorite.component.ts
@@ -1,4 +1,5 @@
-import { Component,OnInit ,Input } from '@angular/core';
+import { Component,OnInit ,Input, Output, EventEmitter  } from '@angular/core';
+
 
 @Component({
     selector:'favorite',
@@ -10,6 +11,7 @@ export class FavoriteComponent implements OnInit{
     //we can also define alias for our input property
     @Input('Fav') isFavorite: boolean;
 
+    @Output('change') click =new EventEmitter();
     //Benefit of using alias is that if the isFavorite varible is changed to some other name like isSelected , then our functionality will be broken where this reusable component is using isFavorite varible ie.app.component.html, so alway use alias 
     //Note : But we have to manually update our favorite.component.html if the variable name is changed 
     ngOnInit(): void {
@@ -22,6 +24,13 @@ export class FavoriteComponent implements OnInit{
 
     onClick(){
         this.isFavorite=!this.isFavorite;
+        //below we are passing a simple boolean value , but if required we can also pass a javascript object containing many key value pairs form our favorite TS class
+        //this.change.emit(this.isFavorite);
+        this.click.emit({isFavorite:this.isFavorite});
     }
    
 }
+
+export interface FavoriteEventArgs{
+    isFavorite :boolean
+  }
--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,4 +1,8 @@
+
 import { Component } from '@angular/core';
+import { FavoriteEventArgs } from './ReusableComponent/favorite.component';
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,5 +14,19 @@ export class AppComponent {
   post={
     title:"Reusable Component",
     isFavorite:true
+  }
+
+ 
+ /*
+ FavoriteEventArgs => Interface for strongly typing the arguments of tis method  =>onFavoriteChanged => Good way to implemnt 
+ we can also simply pass the javascript object having multiple key/value pair but we have to make then strongly typed ie args:{isFavorite:boolean} but if the object is complex then we should define interface in favorite component
+
+ export interface FavoriteEventArgs{
+    isFavorite :boolean
+  }
+ */
+  onFavoriteChanged(args:FavoriteEventArgs){
+    console.log("Favorite changed!!!");
+    console.log(args.isFavorite);
   }
 }
