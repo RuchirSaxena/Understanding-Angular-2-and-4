a5db9877ea5e9af370de8f72f07ee9958b9d7d83
Ruchir Saxena <ruchirsaxena@gmail.com>
2017-10-26 18:54:25 +0530

    Directive *ngFor usage

--- hello-world/src/app/sampledirective/sampledirective.component.ts
+++ hello-world/src/app/sampledirective/sampledirective.component.ts
@@ -15,4 +15,36 @@ export class SampledirectiveComponent  {
         return false;
       }
    }
+
+   coursesData=[
+     {
+       id:1,
+       name:"Angular JS",
+       duration:"1 month"
+     },
+     {
+      id:2,
+      name:"React JS",
+      duration:"3 month"
+     },{
+      id:3,
+      name:"Node JS",
+      duration:"6 month"
+     }
+   ];
+   onAdd(){
+     this.coursesData.push({
+      id:4,
+      name:"Mongo DB",
+      duration:"1 month"
+     });
+   }
+
+   onDelete(course){
+    this.coursesData.splice(course,1);
+   }
+   //USED only when we want to increace the performance of getting data from server
+   trackCourse(index,course){
+      return course?course.id:undefined;
+   }
 }
