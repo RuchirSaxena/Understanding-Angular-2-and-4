9722ee86c9b3877575610d3a0f10202eec0b5752
Ruchir Saxena <ruchir.saxena@irissoftware.com>
2017-11-03 17:49:49 +0530

    Routing Advance

--- hello-world/src/app/users/user/user.component.ts
+++ hello-world/src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
-
-  constructor(private route:ActivatedRoute) { }
+ 
+  constructor(private  route:ActivatedRoute) { }
 
   ngOnInit() {
     this.user={
       id:this.route.snapshot.params['id'],
       name:this.route.snapshot.params['name']
+    };
+ 
+ 
+  this.route.params.subscribe(
+    (params:Params)=>{
+      this.user.id=params['id'];
+      this.user.name=params['name'];
     }
+  );
   }
-
 }
